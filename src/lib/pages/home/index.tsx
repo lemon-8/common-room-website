0c43315ee27b8d8741e8730e53b15542dad0bfae
Shashwat <shashwat4699@gmail.com>
2022-04-20 14:42:49 +0530

    feat(init): setup

---
+++ src/lib/pages/home/index.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Box
+      display={{ md: "flex" }}
+      alignItems="center"
+      minHeight="70vh"
+      gap={8}
+      mb={8}
+      w="full"
+    >
+      Hi
+    </Box>
+  );
+};
+
+export default Home;
